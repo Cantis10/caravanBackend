7a1bafad198c153184a2b3f2f5f59400c5d8ae58
Cantis10 <propertyofcantis@gmail.com>
2026-08-28 16:43:41 +0800

    implemented DB

--- routes/WHATROUTERSLOOKLIKE.js
+++ routes/WHATROUTERSLOOKLIKE.js
@@ -2,10 +2,11 @@ const express = require("express");
 const app = express.Router();
 
 /*
+
+app.customPath = WHATEVER PATH YOU WANT, instead of the file name
 app.get("/THING", (req, res) => {
 });
 
  */
 
 module.exports = app;
-//text
---
+++ routes/tools/databaseChecker.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express.Router();
+const db = require("../../imports/database");
+
+app.customPath = "/dbcheck";
+app.get("/", async (req, res) => {
+  try {
+    const result = await db.execute("SELECT 1 AS test");
+
+    res.json({
+      success: true,
+      database: "working",
+      result: result.rows,
+    });
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      success: false,
+      database: "not working",
+      error: error.message,
+    });
+  }
+});
+
+module.exports = app;
